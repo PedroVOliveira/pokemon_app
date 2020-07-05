5b4ed8c615278be8fb4b41aa9ba6bc0dfe068f49
Pedro <pedro@Macmini-Pedro.local>
2020-07-05 15:52:38 -0300

    Updates

---
+++ precache-manifest.d6b213495d11c587b58ac9a59f54c5bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58a23fd548fae85049cae9be8e55a3ca",
+    "url": "/Pokemon_App/index.html"
+  },
+  {
+    "revision": "58b5584d9185ef260bd2",
+    "url": "/Pokemon_App/static/js/2.3e7d8231.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Pokemon_App/static/js/2.3e7d8231.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5095f9585039906db6e8",
+    "url": "/Pokemon_App/static/js/main.e9503598.chunk.js"
+  },
+  {
+    "revision": "da2308ca516e26f673f1",
+    "url": "/Pokemon_App/static/js/runtime-main.d5cc377e.js"
+  }
+]);
--- src/App/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Routes from '../Pages/routes';
-function App() {
-  return (
-    <Routes />
-  );
-}
-
-export default App;
